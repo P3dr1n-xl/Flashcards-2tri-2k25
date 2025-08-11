c0445aedfed48df31fcaef179d43ceb6cd337119
Pedro Lucas de Bitencourt de Araujo <pedro.bitencourt.araujo@escola.pr.gov.br>
2025-08-11 01:15:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,179 @@
+criaCartao(
+    '1-História',
+    'Qual foi o principal motivo da Revolução Francesa em 1789?',
+    'Desigualdades sociais e econômicas entre os três estados e a crise financeira do país.'
+)
+
+criaCartao(
+    '2-História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889.'
+)
+
+criaCartao(
+    '3-História',
+    'Quem foi o líder da Conjuração Mineira?',
+    'Tiradentes.'
+)
+
+criaCartao(
+    '4-História',
+    'Qual foi o tratado que dividiu as terras do “Novo Mundo” entre Portugal e Espanha em 1494?',
+    'Tratado de Tordesilhas.'
+)
+
+criaCartao(
+    '5-História',
+    'Durante a Idade Média, qual era o sistema econômico predominante na Europa Ocidental?',
+    'Feudalismo.'
+)
+
+criaCartao(
+    '6-História',
+    'Qual civilização construiu Machu Picchu?',
+    'Civilização Inca.'
+)
+
+criaCartao(
+    '7-Geografia',
+    'Qual é o bioma predominante na região Norte do Brasil?',
+    'Floresta Amazônica.'
+)
+
+criaCartao(
+    '8-Geografia',
+    'Qual país possui o maior território do mundo?',
+    'Rússia.'
+)
+
+criaCartao(
+    '9-Geografia',
+    'Qual é o rio mais extenso do mundo?',
+    'Rio Nilo.'
+)
+
+criaCartao(
+    '10-Geografia',
+    'O que é latitude?',
+    'Distância, em graus, de um ponto em relação à Linha do Equador.'
+)
+
+criaCartao(
+    '11-Geografia',
+    'Qual é o deserto mais seco do mundo?',
+    'Deserto do Atacama.'
+)
+
+criaCartao(
+    '12-Geografia',
+    'Qual oceano banha a costa leste do Brasil?',
+    'Oceano Atlântico.'
+)
+
+criaCartao(
+    '13-Matemática',
+    'Qual é a fórmula da área de um triângulo?',
+    'A=B⋅h/2 (base x altura divido por 2)'
+)
+
+criaCartao(
+    '14-Matemática',
+    'Quanto é 7^2 (elevado)?',
+    '49.'
+)
+
+criaCartao(
+    '15-Matemática',
+    'O que significa MMC?',
+    'Mínimo Múltiplo Comum.'
+)
+
+criaCartao(
+    '16-Matemática',
+    'Qual o valor de 3/4 (Três quartos) em %?',
+    '75%.'
+)
+
+criaCartao(
+    '17-Matemática',
+    'Qual a área de um quadrado de lado 5cm?',
+    '25cm2.'
+)
+
+criaCartao(
+    '18-Matemática',
+    'Se um produto custa R$ 80,00 e sofre um desconto de 25%, qual será o preço final?',
+    'R$ 60,00.'
+)
+
+criaCartao(
+    '19-Esportes',
+    'Qual país sediou a Compa do Mundo de Futebol em 2022?',
+    'Qatar.'
+)
+
+criaCartao(
+    '20-Esportes',
+    'Quantos jogadores cada time possui em quadra durante uma partida oficial de Basquete?',
+    '5 jogadores.'
+)
+
+criaCartao(
+    '21-Esportes',
+    'Como se chama o torneio de tenis disputado em Wimbledon?',
+    'The Champions Wimbledon.'
+)
+
+criaCartao(
+    '22-Esportes',
+    'O que é considerado uma rotação no Vôlei?',
+    'É o movimento dos jogadores em sentido horário após ganho de ponto.'
+)
+
+criaCartao(
+    '23-Esportes',
+    'Quais são os 4 estilos de nado utilizados em uma prova de natação medley?',
+    'Borboleta, Costas, Bruços e Crawl.'
+)
+
+criaCartao(
+    '24-Esportes',
+    'Qual é o atleta que é considerado o homem mais rápido do mundo nas prova de 100 e 200 metros rasos?',
+    'Usain Bolt.'
+)
+
+criaCartao(
+    '25-Ficção',
+    'Em "O rei leão", qual o nome do pai de Simba?',
+    'Mufasa..'
+)
+
+criaCartao(
+    '26-Ficção',
+    'No filme Frozen, qual é o poder mágico da rainha Elsa?',
+    'Poder de criar gelo e neve.'
+)
+
+criaCartao(
+    '27-Ficção',
+    'No filme Toy Story, qual é o nome do Cowboy que é o brinquedo favorito de Andy?',
+    'Woody.'
+)
+
+criaCartao(
+    '28-Ficção',
+    'Em procurando Nemo, qual é o nome do pai de Nemo?',
+    'Marlin.'
+)
+
+criaCartao(
+    '29-Ficção',
+    'Qual é o nome da princesa que perde o sapato de cristal em um baile real?',
+    'Cinderela.'
+)
+
+criaCartao(
+    '30-Ficção',
+    'No filme Divertidamente, quais são as 5 emoções principais na cabeça da Riley?',
+    'Alegria, tristeza, raiva, medo e nojinho.'
+)
